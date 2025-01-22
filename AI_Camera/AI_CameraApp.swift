ffad6f7c40706a37521d49655f39ba4820f05e04
sszz01 <resios027@gmail.com>
2025-01-21 23:02:12 -0800

    Initial Commit

---
+++ AI_Camera/AI_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_CameraApp.swift
+//  AI_Camera
+//
+//  Created by Oleksandr Voievodin on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI_Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI_Camera
+//
+//  Created by Oleksandr Voievodin on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
