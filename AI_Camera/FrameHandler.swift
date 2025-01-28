6565ef21b0d574605cdc213f2899428b1821a81e
sszz01 <resios027@gmail.com>
2025-01-27 23:43:24 -0800

    FrameHandler and FrameView added for dynamic camera preview. User can now preview camera by tapping the button in the ContentView

---
+++ AI_Camera/FrameHandler.swift
@@ -0,0 +1,8 @@
+//
+//  FrameHandler.swift
+//  AI_Camera
+//
+//  Created by Oleksandr Voievodin on 1/27/25.
+//
+
+import Foundation
---
+++ AI_Camera/FrameView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameView.swift
+//  AI_Camera
+//
+//  Created by Oleksandr Voievodin on 1/27/25.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameView()
+}
