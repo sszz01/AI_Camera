6521480cfe0f1a7cf68979528362eaeefeb52e1f
sszz01 <resios027@gmail.com>
2025-01-29 22:51:32 -0800

    white background adjusted to secondary gray

--- AI_Camera/ContentView.swift
+++ AI_Camera/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.white
+                Color.secondary
                     .ignoresSafeArea()
                 
                 VStack {
